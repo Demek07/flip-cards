ed3c7f5efc0eb73de96c80187e3ada7b363cf742
d_e_m_e_k <136734449+Demek07@users.noreply.github.com>
2024-07-07 12:25:47 +0300

    Update settings.py

--- flip_cards/settings.py
+++ flip_cards/settings.py
@@ -28,13 +28,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if socket.gethostname() == '127.0.0.1':
+#if socket.gethostname() == '127.0.0.1':
 
-    DEBUG = True
+    #DEBUG = True
 
-else:
-
-    DEBUG = False
+#else:
+DEBUG = False
 
 ALLOWED_HOSTS = ['flip-cards.ru', 'www.flip-cards.ru', 'localhost', '127.0.0.1', '213.171.10.225']
 
