fc7b16cf4e7abf4934e08fcef644a7e0798d96e3
Demek07 <136734449+Demek07@users.noreply.github.com>
2025-04-03 11:06:55 +0300

    обновил настройки под Postgre

--- flip_cards/settings.py
+++ flip_cards/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['flip-cards.ru', 'www.flip-cards.ru', 'localhost',
-                 '127.0.0.1', '176.124.215.68', 'demek07-flip-cards-f779.twc1.net']
+                 '127.0.0.1', '188.225.25.251', 'demek07-flip-cards-66be.twc1.net', '192.168.0.6']
 
 CSRF_TRUSTED_ORIGINS = ['https://flip-cards.ru', 'https://www.flip-cards.ru']
 
